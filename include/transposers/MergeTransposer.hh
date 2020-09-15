3e31d52052b1e166ed781b4d8d5440f6ce8d44dc
Massimiliano Incudini <massimiliano.incudini@outlook.com>
2020-09-15 16:57:18 +0200

    cazz

--- include/transposers/MergeTransposer.hh
+++ include/transposers/MergeTransposer.hh
@@ -8,86 +8,184 @@
 #define DIV_THEN_CEIL(a,b) a%b?((a/b)+1):(a/b)
 #endif
 
-class sort_indices
-{
-    private:
-        int* mparr;
+void compress_pointers(int n, int start_index, int end_index, int const idx[], int ptr[]) {
 
+    for(int j = start_index; j < end_index; j++) { 
+        ptr[idx[j] + 1]++;
+    } 
+    for(int j = 1; j < n + 1; j++) { // prefix_sum
+        ptr[j] += idx[j-1];
+    }
+}
+
+void compress_pointers_all(int n, int nnz, int n_thread, 
+    int const * colIdx, int *blockColPtr) {
+
+    int len = DIV_THEN_CEIL(nnz, n_thread);
+
+    for(int i = 0; i < n_thread; i++) {
+
+        int start_index = i*len;
+        int end_index = (i+1)*len < nnz ? (i+1)*len : nnz;
+        int current_len = end_index - start_index;
+
+        int* colPtr = blockColPtr + (i * (n+1));
+        compress_pointers(n, start_index, end_index, colIdx, colPtr);
+    }
+}
+
+void sort_for_column(
+    int nnz, int N_THREAD, 
+    int const * colIdxIn, int const * rowIdxIn, float const * valIn,
+    int *colIdxOut, int *rowIdxOut, float *valOut
+) {
+
+    class sort_indices {
+    private:
+        const int const * mparr;
     public:
-        sort_indices(int* parr) : mparr(parr) {}
+        sort_indices(const int const * parr) : mparr(parr) { }
         bool operator()(int i, int j) const { return mparr[i]<mparr[j]; }
-};
+    };
+
+    int len = DIV_THEN_CEIL(nnz, N_THREAD);
+    int* indices = new int[len];
+
+    for(int i = 0; i < N_THREAD; i++) {
+
+        int start_index = i*len;
+        int end_index = (i+1)*len < nnz ? (i+1)*len : nnz;
+        int current_len = end_index - start_index;
+
+        // riordino i blocchi facendo il sort
+        for(int j = 0; j < current_len; j++) { indices[j] = j; }
+
+        std::sort(indices+start_index, indices+end_index, sort_indices(colIdxIn));
+        
+        for(int j = start_index; j < end_index; j++) {
+            colIdxOut[j] = colIdxIn[indices[j]]; // OK perchè i due array sono diversi
+            rowIdxOut[j] = rowIdxIn[indices[j]]; 
+            valOut[j] = valIn[indices[j]];
+        }
+    }
+
+}
+
+void merge_pcsc_serial(
+    int begin, int end,
+    int const colPtrA[], int const rowIdxA[], float const valA[],
+    int const colPtrB[], int const rowIdxB[], float const valB[],
+    int colPtrC[], int rowIdxC[], int valC[])
+{
+    for(int i = begin; i < end+1; i++) {
+        colPtrC[i] = colPtrA[i] + colPtrB[i];
+    }
+    for(int i = begin; i < end; i++) {
+        int sa = colPtrA[i], la = colPtrA[i+1] - sa;
+        int sb = colPtrB[i], lb = colPtrB[i+1] - sb;
+        int sc = colPtrC[i], lc = colPtrC[i+1] - sc;
+
+        for(int j = 0; j < la; j++) {
+            rowIdxC[sc+j] = rowIdxA[sa + j];
+            valC[sc+j] = valA[sa + j];
+        }
+        sc = sc + la;
+        for(int j = 0; j < la; j++) {
+            rowIdxC[sc+j] = rowIdxC[sa + j];
+            valC[sc+j] = valC[sa + j];
+        }
+    }
+}
 
 class MergeTransposer : public AbstractTransposer
 {
 
 private:
 
-    
+    struct merge_buffer {
+        const int n, nnz, n_thread;
+        int* blockColPtr;
+        int* rowIdx;
+        float* val;
+        merge_buffer(int n, int nnz, int n_thread) : n(n), nnz(nnz), n_thread(n_thread) { }
+        void allocate() {
+            blockColPtr = new int[n_thread * (n+1)];
+            rowIdx = new int[nnz];
+            val = new float[nnz];
+        }
+        void deallocate() {
+            delete blockColPtr, rowIdx, val;
+        }
+        int* getColPtr(int thread) {
+            return blockColPtr + (thread * (n+1));
+        }
+    };
 
     const int N_THREAD = 100;
 
     virtual int csr2csc(
         int m, int n, int nnz, 
-        int* csrRowPtr, int* csrColIdx, float* csrVal, 
+        int const csrRowPtr[], int const csrColIdx[], float const csrVal[], 
         int* cscColPtr, int* cscRowIdx, float* cscVal) 
     {
+        merge_buffer ping(n, nnz, N_THREAD), pong(n, nnz, N_THREAD);
+        ping.allocate();
+        pong.allocate();
 
         // 1. espando `rowPtr` per farlo diventare `rowIdx`
-        int* rowIdx = new int[nnz];
         for(int j = 0; j < m; j++) {
             for(int i = csrRowPtr[j]; i < csrRowPtr[j+1]; i++) {
-                rowIdx[i] = j;
+                ping.rowIdx[i] = j;
             }
         }
 
-        // 2. creo i vari blocchi
-        int* blockColPtr = new int[N_THREAD * (n+1)](); // () inizializza a zero tutto
+        // 2. riempio i vari blocchi - seriale
+        {
+            int* colIdxSorted = new int[nnz];
+            std::memcpy(colIdxSorted, csrColIdx, nnz*sizeof(int));
 
-        // 3. riempio i vari blocchi - seriale
-        int* colIdxSorted = new int[nnz];
-        std::memcpy(colIdxSorted, csrColIdx, nnz*sizeof(int));
+            // sorting dei valori
+            sort_for_column(nnz, N_THREAD, 
+                csrColIdx, ping.rowIdx, csrVal,
+                colIdxSorted, pong.rowIdx, pong.val);
 
-        int len = DIV_THEN_CEIL(nnz, N_THREAD);
-        int* indices = new int[len];
+            // riempio `blockColPtr` di pong
+            compress_pointers_all(n, nnz, N_THREAD, colIdxSorted, pong.blockColPtr);
 
-        for(int i = 0; i < N_THREAD; i++) {
+            delete colIdxSorted;
+        }
 
-            int start_index = i*len;
-            int end_index = (i+1)*len < nnz ? (i+1)*len : nnz;
-            int current_len = end_index - start_index;
+        // 3. `ping` vuoto, `pong` ha valori validi
+        merge_buffer buffers[2] = { ping, pong };
+        int full = 1;
 
-            // riordino i blocchi facendo il sort
-            for(int j = 0; j < current_len; j++) { 
-                indices[j] = j;
+        // 4. chiamo il merge
+        for(int nblocks = N_THREAD; nblocks >= 2; nblocks /= 2)
+
+            for(int i = 0; i < nblocks; i++) {
+
+                /* int begin, int end,
+    int const colPtrA[], int const rowIdxA[], float const valA[],
+    int const colPtrB[], int const rowIdxB[], float const valB[],
+    int colPtrC[], int rowIdxC[], int valC[] */
+                int len = DIV_THEN_CEIL(nnz, nblocks);
+                int begin = i * len;
+                int end = (i+1) * len; if(end > nnz) { end = nnz; }
+
+                merge_pcsc_serial(begin, end, 
+                    buffers[full].blockColPtr + ..., buffers[full].rowIdx + ..., buffers[full].val + ...,
+                    buffers[full].blockColPtr + ..., buffers[full].rowIdx + ..., buffers[full].val + ...,
+                    buffers[1-full].blockColPtr + ..., buffers[1-full].rowIdx + ..., buffers[1-full].val + ...
+                );
             }
 
-            std::sort(indices+start_index, indices+end_index, sort_indices(indices));
-            
-            for(int j = start_index; j < end_index; j++) {
-                colIdxSorted[j] = csrColIdx[indices[j]]; // OK perchè i due array sono diversi
-                cscRowIdx[j] = rowIdx[indices[j]]; 
-                cscVal[j] = csrVal[indices[j]];
-            }
-
-            // creo il `colPtr` dal `colIdxSorted` corrente
-            int* currentBlockColPtr = blockColPtr + i*(nnz+1);
-
-            for(int j = start_index; j < end_index; j++) {
-                currentBlockColPtr[colIdxSorted[j] + 1]++;
-            }
-            for(int j = 1; j < n + 1; j++) { // prefix_sum
-                currentBlockColPtr[j] += currentBlockColPtr[j-1];
-            }
-
+            full = 1-full; // ping pong della memoria
         }
 
 
         // deallocazione
-        delete rowIdx;
-        delete colIdxSorted;
-        delete blockColPtr;
-        delete indices;
+        ping.deallocate();
+        pong.deallocate();
     }
 
 public:
