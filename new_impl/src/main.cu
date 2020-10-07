830476b2fc586d5ad0e15ffda4ef7d313d93ea47
michelepenzo <michelepenzo@outlook.it>
2020-10-07 18:24:04 +0200

    iniziato report

--- new_impl/src/main.cu
+++ new_impl/src/main.cu
@@ -27,7 +27,6 @@ int main(int argc, char **argv) {
  // ok = transposer::component_test::segmerge();
  // std::cout << "segmerge     OK: " << ok << "\n";
 
-<<<<<<< HEAD
     // ok = transposer::component_test::segmerge3();
     // std::cout << "segmerge3    OK: " << ok << "\n";
 
@@ -36,16 +35,6 @@ int main(int argc, char **argv) {
 
     // ok = transposer::component_test::segmerge3_sm();
     // std::cout << "segmerge3_sm OK: " << ok << "\n";
-=======
- // ok = transposer::component_test::segmerge3();
- // std::cout << "segmerge3    OK: " << ok << "\n";
-
-//  ok = transposer::component_test::segmerge_sm();
-//  std::cout << "segmerge_sm  OK: " << ok << "\n";
-    
-    ok = transposer::component_test::segmerge3_sm();
-    std::cout << "segmerge3_sm OK: " << ok << "\n";
->>>>>>> 26b004723b929281ec32a8e1e0a9a45c60ebbc34
 
 //  ok = transposer::component_test::sort();
 //  std::cout << "sort OK: " << ok << "\n";
